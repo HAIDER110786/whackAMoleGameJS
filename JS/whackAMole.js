3335b2b6c50535021ca998f05c49f0aba2c96b92
syed haider imam abidi <abidihaider89@gmail.com>
2020-08-06 03:08:08 +0500

    bugFixedInProject

--- JS/whackAMole.js
+++ JS/whackAMole.js
@@ -1,5 +1,7 @@
 const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('.startButton button');
+const reset = document.querySelector('.reset button');
 const timer = document.querySelector('.timer');
 const score = document.querySelector('.score');
 let clock;
@@ -8,23 +10,25 @@ let GameTimer;
 let points = 0;
 let i;
 
-holes.forEach(hole => {
-    hole.addEventListener('click',updateTheScore);
-})
+moles.forEach(mole => {
+    mole.addEventListener('click',updateTheScore);
+});
+
 
 function updateTheScore(e){
-    if(e.isTrusted){
-        points++;
-        score.textContent = `Score : ${points} ${points===1 ? 'point' : 'points'}`;
-        holes[i].classList.remove('show');
-    }
+    points++;
+    score.textContent = `Score : ${points} ${points===1 ? 'point' : 'points'}`;
+    holes[i].classList.remove('show');
 }
 
 startButton.addEventListener('click',startTheGame);
+reset.addEventListener('click',()=>window.location.reload());
 
 function startTheGame(){
+    startButton.disabled = true;
     startTheTimer();
     startShowingThemoles();
+    score.textContent = 'Score : 0 points';
 }
 
 function startShowingThemoles() {
@@ -34,21 +38,27 @@ function startShowingThemoles() {
         setTimeout(() => {
             holes[i].classList.remove('show');
             startShowingThemoles();
-        }, 600);
+        }, 1000);
     }
     else{
         clearInterval(GameTimer);
         holes[i].classList.remove('show');
+        clock=0;
+        endTheGame = false;
+        points = 0;
+        startButton.disabled = false;
+        startButton.textContent = 'Play Again';
+        alert('game over');
     }
 }
 
 function startTheTimer(){
     clock = 15;
-    timer.textContent = '00:15';
+    timer.textContent = 'Timer : 00:15';
     GameTimer = setInterval(() => {
         clock--;
         if(clock>=0){
-            timer.textContent = `00:${clock>=10 ? clock : '0'+clock}`;
+            timer.textContent = `Timer : 00:${clock>=10 ? clock : '0'+clock}`;
         }
         else{
             endTheGame=true;
